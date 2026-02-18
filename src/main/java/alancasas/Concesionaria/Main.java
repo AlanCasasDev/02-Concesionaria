e91f37d113c312d6c57b03fa6a24388355d0cb7f
AlanCasas <aacf2005@gmail.com>
2026-02-18 13:08:09 -0300

    Commit final

--- src/main/java/alancasas/Concesionaria/Main.java
+++ src/main/java/alancasas/Concesionaria/Main.java
@@ -10,8 +10,8 @@ public class Main {
 
     public static void main(String[] args) {
         
-        //Cambio para agregar al commit en el branch
-        System.out.println("Este cambio es el ultimo y confirma el merge con prueba1");
+        
+        System.out.println("Hola mundo!");
         
         Home pantallaHome = new Home();
         pantallaHome.setVisible(true);
