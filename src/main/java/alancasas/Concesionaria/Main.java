ed53a9d1783c60f83dfdb0548c628c927002f5c1
AlanCasas <aacf2005@gmail.com>
2026-02-18 13:00:27 -0300

    Segundo cambio realizado en la rama prueba1

--- src/main/java/alancasas/Concesionaria/Main.java
+++ src/main/java/alancasas/Concesionaria/Main.java
@@ -11,7 +11,7 @@ public class Main {
     public static void main(String[] args) {
         
         //Cambio para agregar al commit en el branch
-        System.out.println("Hello World!");
+        System.out.println("Este es el segundo cambio para la nueva rama prueba1");
         
         Home pantallaHome = new Home();
         pantallaHome.setVisible(true);
